c07cf82fbdfaabe7ab86c940a9269d6086eba0f8
DeepakSingh9 <indeepak9@gmail.com>
2019-07-06 19:05:08 +0530

    'extend'

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jt$28cnln1lenjjqsv7asm*^1lx!%^=x*0p2(qt*8_7hr4r1t6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dscript.pythonanywhere.com']
+ALLOWED_HOSTS = ['dscript.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
